c4f9d4ad29839628c31cd031ac634c9a260d92c3
Jake Esperson <jake.esperson@outlook.com>
2023-11-11 16:15:48 -0800

    fixed main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,15 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import VueGoogleMaps from '@fawmi/vue-google-maps'
-
 const app = createApp(App);
-app.use(VueGoogleMaps, {
-    load: {
-        key: 'YOUR_API_KEY_COMES_HERE',
-        // language: 'de',
-    },
-}).mount('#app')
-
 
 createApp(App).mount('#app')
