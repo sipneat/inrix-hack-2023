22427bdbe31fd0b47d0130d30f1215eba7e36987
Catherine-H1 <65926387+Catherine-H1@users.noreply.github.com>
2023-11-11 23:44:05 -0800

    moved search bar

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -13,10 +13,27 @@
     <div style="width: 595px; padding-left: 8px; padding-right: 8px; left: 185px; top: 221px; position: absolute; background: white; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.16); border-radius: 24px; overflow: hidden; justify-content: flex-start; align-items: flex-start; gap: 8px; display: inline-flex">
         <img style="width: 40px; height: 40px" src="https://s3-alpha-sig.figma.com/img/2503/8781/6ba33e4e01d59ea6f3e0b1f4aa06d766?Expires=1700438400&Signature=iY6l3mOyKAhYSbcLxMxmYKUVTTzjT1x0B8H3mlbFgMxNOJa9GYnS8aNY4wBXe9oqZBE5fd5dEjqndWc8qxz4R5qLVp9k2UTIfOitWpQZ-8Lcw8NxfZ8Y2RgtmaAxF~kjAseLegrMHowquAvSivWFNMdGhKxGn3Ro0CwRkICPlSpqEA1yMxkyjHFE0ecVWGQl~tjUetyOtO-HdgP9VoaQwACJ6dGyL9g73GdSGTIqP9hUmRfPaX8vheZRZSP2uNgn5vZKTq9whyPUvdmj3VpbOdjxwdw68Oh6S~HwRANISKwSn2tJ8A2toq2qm9UBA7dG4jDX3Njr11-UQ5s~YvoS1w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
         <input class="search" type = "text" id = "location" location = "location" placeholder = "ex: Golden Gate Brg">
-        <button style="width: 70px; height: 40px" class="button" type="submit">Submit</button>
+        <!--<button style="width: 70px; height: 40px" class="button2" type="submit">Submit</button> -->
         <div style="width: 225px; height: 40px; opacity: 0.30; color: black; font-size: 30px; font-family: Inter; font-weight: 400; word-wrap: break-word"><br/><br/><br/>Enter Location</div>
     </div>
-    <div style="width: 302px; height: 6px; left: 945px; top: 418px; position: absolute; background: #EEEEEE; border-radius: 100px"></div>
+
+    <div style="left: 745px; top: 215px; position: absolute">
+        <button class="button2" type="submit">Submit</button>
+    </div>
+    
+    
+        
+    
+
+    <div style="width: 302px; height: 6px; left: 945px; top: 418px; position: absolute; background: #EEEEEE; border-radius: 100px">   </div>
+    
+
+
+    
+
+
+
+
     <div style="height: 436px; left: 952px; top: 488px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 55px; display: inline-flex">
         <div style="width: 288px; text-align: center; color: white; font-size: 32px; font-family: Kanit; font-weight: 600; line-height: 40px; word-wrap: break-word">Selected Parking:</div>
         <div style="justify-content: flex-start; align-items: center; gap: 15px; display: inline-flex">
@@ -47,20 +64,21 @@
             <!-- <div style="width: 67px; height: 48px; padding: 10px; background: #64CCC5; border-radius: 15px; justify-content: center; align-items: center; gap: 10px; display: flex">
                 <div style="text-align: center; color: #EEEEEE; font-size: 20px; font-family: Kanit; font-weight: 400; line-height: 20px; word-wrap: break-word">0.2</div>
             </div>-->
-            <button class="button" type="submit">0.2</button>
+            
+            <button class="button1" type="submit">0.2</button>
             <!--<div style="width: 67px; height: 48px; padding: 10px; background: #64CCC5; border-radius: 15px; justify-content: center; align-items: center; gap: 10px; display: flex">
                 <div style="text-align: center; color: #EEEEEE; font-size: 20px; font-family: Kanit; font-weight: 400; line-height: 20px; word-wrap: break-word">0.5</div>
             </div> -->
-            <button class="button" type="submit">0.5</button>
+            <button class="button1" type="submit">0.5</button>
             <!-- <div style="width: 67px; height: 48px; padding: 10px; background: #64CCC5; border-radius: 15px; justify-content: center; align-items: center; gap: 10px; display: flex">
                 <div style="text-align: center; color: #EEEEEE; font-size: 20px; font-family: Kanit; font-weight: 400; line-height: 20px; word-wrap: break-word">0.7</div>
             </div> --> 
-            <button class="button" type="submit">0.7</button>
+            <button class="button1" type="submit">0.7</button>
 
            <!-- <div style="width: 67px; height: 48px; padding: 10px; background: #64CCC5; border-radius: 15px; justify-content: center; align-items: center; gap: 10px; display: flex">
                 <div style="text-align: center; color: #EEEEEE; font-size: 20px; font-family: Kanit; font-weight: 400; line-height: 20px; word-wrap: break-word">1.0</div>
             </div> -->
-            <button class="button" type="submit">1.0</button>
+            <button class="button1" type="submit">1.0</button>
         </div>
     </div>
 </div>
